d03cb77e75d5abd4329005178c3a6ac0c4099ba7
Schuyler Klaassen and paul hess <student@epicodus.com>
2016-03-16 10:00:52 -0700

    add api call for bikeIndex

--- build/js/app.js
+++ build/js/app.js
@@ -2,4 +2,13 @@
 
 
 
+$(document).ready(function() {
+	var zip = 97204;
+	$.get('https://bikeindex.org:443/api/v2/bikes_search/stolen?page=1&proximity=' + zip + '&proximity_square=100&stolen_after=1451494024', function(response) {
+		console.log(response);
+
+		$('body').append('<div></div>').text(response.bikes[0].date_stolen);
+	});
+});
+
 },{}]},{},[1]);
