e3b45d620c3cca63c83f520efa5294b158a1a36d
paul hess and Schuyler Klaassen <pauljacksonhess@gmail.com>
2016-03-16 19:51:46 -0700

    add connection of bike api to goog map api

--- .tmp/allConcat.js
+++ .tmp/allConcat.js
@@ -1,18 +1,48 @@
 
 
 var googApiKey = require('./../.env').googApiKey;
+var geocoder;
+var map;
+var i = 0;
+
+function myLoop(object) {
+	setTimeout(function() {
+		console.log(object);
+		placeMarkers(object, i);
+		i++;
+		if (i < object.bikes.length) {
+			myLoop(object);
+		}
+	}, 201)
+}
+function placeMarkers(bikeData, i) {
+	// console.log(bikeData);
+	geocoder.geocode( { 'address': bikeData.bikes[i].stolen_location}, function(results, status) {
+		if (status == google.maps.GeocoderStatus.OK) {
+			var marker = new google.maps.Marker({
+				map: map,
+				position: results[0].geometry.location
+			});
+		} else {
+			alert("Geocode was not successful for the following reason: " + status);
+		}
+	});
 
 
+}
 $(document).ready(function() {
-	var geocoder;
-	var map;
+
+
+
+
 	function initialize() {
     geocoder = new google.maps.Geocoder();
-    var latlng = new google.maps.LatLng(-34.397, 150.644);
+    var latlng = new google.maps.LatLng(39.8282, -98.5795);
     var mapOptions = {
-      zoom: 8,
+      zoom: 5,
       center: latlng
-    }
+    };
+
     map = new google.maps.Map(document.getElementById("map"), mapOptions);
   }
  initialize();
@@ -48,10 +78,10 @@ $(document).ready(function() {
 
 	console.log('foo');
 
-	var zip = 97215;
-	$.get('https://bikeindex.org:443/api/v2/bikes_search/stolen?page=1&proximity=' + zip + '&proximity_square=10&stolen_after=1451494024', function(response) {
-		console.log(response);
-
+	var zip = 69036;
+	$.get('https://bikeindex.org:443/api/v2/bikes_search/stolen?page=1&per_page=500&proximity=69036&proximity_square=3000&stolen_after=1451666936', function(response) {
+		// console.log(response);
+		myLoop(response);
 		$('body').append("<div>" + response.bikes[0].stolen_location + "</div>");
 
 	});
